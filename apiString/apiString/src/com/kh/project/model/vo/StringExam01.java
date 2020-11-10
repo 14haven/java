f2f9f9f1c3ae1d4d75dd9b2d68e389a58f35aeec
14haven <73207547+14haven@users.noreply.github.com>
2020-11-10 22:54:40 +0900

    Add files via upload

---
+++ apiString/apiString/src/com/kh/project/model/vo/StringExam01.java
@@ -0,0 +1,10 @@
+package com.kh.project.model.vo;
+
+public class StringExam01 {
+	public void stringTest() {
+		String s1 = "Hello"; // 문자열은 객체처럼 다른 영역에 저장된다.
+		s1 = "hihi";
+		System.out.println(s1);
+		String ss1 = new String();
+	}
+}
